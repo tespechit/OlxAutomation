a1f031a001a0c96216faecb6b3eaeec44a6e267c
Fernando Ribeiro Salvador de Sa <ribeiro.rs@gmail.com>
2018-06-05 18:10:13 -0300

    First stable version

--- OlxAutomation/Form1.cs
+++ OlxAutomation/Form1.cs
@@ -20,7 +20,9 @@ namespace OlxAutomation
         IWebDriver driver;
         string lastmessage;
         ChromeOptions options = new ChromeOptions();
-        
+
+        List<Anuncio> lstanounces = new List<Anuncio>();
+
 
         public Form1()
         {
@@ -38,49 +40,61 @@ namespace OlxAutomation
                 GetAnounces();
                 Thread.Sleep(2000);
             }
-            
+
         }
 
         private void OpenOlx()
         {
             //driver = new ChromeDriver(options);
-            
+
         }
 
         private void GetAnounces()
         {
             List<IWebElement> lst = new List<IWebElement>();
-            List<string> listmessages = new List<string>();
+            List<Anuncio> listmessages = new List<Anuncio>();
 
             again:
             try
             {
+                driver = new ChromeDriver(options);
+                driver.Navigate().GoToUrl("https://sp.olx.com.br/sao-paulo-e-regiao/zona-norte/santana");
 
-            
-            driver = new ChromeDriver(options);
-            driver.Navigate().GoToUrl("https://sp.olx.com.br/sao-paulo-e-regiao/zona-norte/santana");
+                lst = driver.FindElements(By.ClassName("OLXad-list-link")).ToList();
 
-            lst = driver.FindElements(By.ClassName("OLXad-list-link")).ToList();
+                foreach (IWebElement elem in lst)
+                {
+                    listmessages.Add(new Anuncio { Title = elem.GetAttribute("href"), Texto = elem.Text });
+                }
 
-            foreach(IWebElement elem in lst)
-            {
-                listmessages.Add(elem.Text);
-            }
+                foreach (Anuncio msg in listmessages)
+                {
+                    bool alreadyExists = lstanounces.Any(x => x.Title == msg.Title);
 
-            string message = listmessages.Last();
+                    if (!alreadyExists)
+                    {
+                        lstanounces.Add(msg);
+                        SendMail(msg.Texto, msg.Title);
+                    }
 
-            if(lastmessage == message)
-            {
-                //MessageBox.Show("Não há novos anúncios");
-            }
-            else
-            {
-                SendMail("Novo Item no OLX em Santana", message);
-                lastmessage = listmessages.Last();
-            }
 
-            driver.Close();
-            driver.Dispose();
+                }
+
+
+                string message = listmessages.Last().Title;
+
+                if (lastmessage == message)
+                {
+                    //MessageBox.Show("Não há novos anúncios");
+                }
+                else
+                {
+
+                    lastmessage = listmessages.Last().Title;
+                }
+
+                driver.Close();
+                driver.Dispose();
             }
             catch
             {
@@ -93,20 +107,51 @@ namespace OlxAutomation
 
         private static void SendMail(string subject, string message)
         {
-            MailMessage objeto_mail = new MailMessage();
-            SmtpClient client = new SmtpClient();
-            client.Port = 587;
-            client.Host = "smtp.gmail.com";
-            client.Timeout = 10000;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.EnableSsl = true;
-            client.Credentials = new System.Net.NetworkCredential("ribeiro.rs@gmail.com", "c299792458MS");
-            objeto_mail.From = new MailAddress("ribeiro.rs@gmail.com");
-            objeto_mail.To.Add(new MailAddress("ribeiro.rs@gmail.com"));
-            objeto_mail.Subject = subject;
-            objeto_mail.Body = message;
-            client.Send(objeto_mail);
+            //string mensagemtratada = subject;
+            //int posmsg = mensagemtratada.
+            string valor = subject;
+            try
+            {
+
+                string mensagemtratada = subject;
+                int hasValue = subject.IndexOf("R$");
+                int teste2 = subject.IndexOf("\r\n");
+                string infoanuncio = "";
+
+                if (hasValue > 0)
+                {
+                    infoanuncio = subject.Substring(hasValue, subject.Length - hasValue);
+                    infoanuncio = infoanuncio.Replace("\r\n", "-");
+                    //mensagemtratada = subject.Replace("\r\n", string.Empty);
+                }
+                else
+                {
+
+                }
+
+                mensagemtratada = subject.Replace("\r\n", " - ");
+                //mensagemtratada = mensagemtratada.Substring(1, mensagemtratada.IndexOf("São Paulo, Santana") - 1);
+                //mensagemtratada.Substring();
+                MailMessage objeto_mail = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.EnableSsl = true;
+                client.Credentials = new System.Net.NetworkCredential("ribeiro.rs@gmail.com", "c299792458MS");
+                objeto_mail.From = new MailAddress("ribeiro.rs@gmail.com");
+                objeto_mail.To.Add(new MailAddress("ribeiro.rs@gmail.com"));
+                objeto_mail.Subject = mensagemtratada;
+                objeto_mail.Body = message;
+                client.Send(objeto_mail);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
 
         }
     }
