82e28163c8a04c7d6b8eea171e303ebc3bd4f932
Fernando Ribeiro Salvador de Sa <ribeiro.rs@gmail.com>
2018-06-05 11:41:46 -0300

    Add project files.

---
+++ OlxAutomation/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OlxAutomation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtoMonitor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtoMonitor
+            // 
+            this.BtoMonitor.Location = new System.Drawing.Point(12, 12);
+            this.BtoMonitor.Name = "BtoMonitor";
+            this.BtoMonitor.Size = new System.Drawing.Size(138, 64);
+            this.BtoMonitor.TabIndex = 0;
+            this.BtoMonitor.Text = "Monitorar Olx";
+            this.BtoMonitor.UseVisualStyleBackColor = true;
+            this.BtoMonitor.Click += new System.EventHandler(this.BtoMonitor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtoMonitor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtoMonitor;
+    }
+}
+
---
+++ OlxAutomation/Form1.cs
@@ -0,0 +1,113 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlxAutomation
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver;
+        string lastmessage;
+        ChromeOptions options = new ChromeOptions();
+        
+
+        public Form1()
+        {
+            options.AddArguments("--headless");
+            InitializeComponent();
+        }
+
+        private void BtoMonitor_Click(object sender, EventArgs e)
+        {
+            //SendMail("Teste", "Teste message");
+            //OpenOlx();
+
+            while (true)
+            {
+                GetAnounces();
+                Thread.Sleep(2000);
+            }
+            
+        }
+
+        private void OpenOlx()
+        {
+            //driver = new ChromeDriver(options);
+            
+        }
+
+        private void GetAnounces()
+        {
+            List<IWebElement> lst = new List<IWebElement>();
+            List<string> listmessages = new List<string>();
+
+            again:
+            try
+            {
+
+            
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://sp.olx.com.br/sao-paulo-e-regiao/zona-norte/santana");
+
+            lst = driver.FindElements(By.ClassName("OLXad-list-link")).ToList();
+
+            foreach(IWebElement elem in lst)
+            {
+                listmessages.Add(elem.Text);
+            }
+
+            string message = listmessages.Last();
+
+            if(lastmessage == message)
+            {
+                //MessageBox.Show("Não há novos anúncios");
+            }
+            else
+            {
+                SendMail("Novo Item no OLX em Santana", message);
+                lastmessage = listmessages.Last();
+            }
+
+            driver.Close();
+            driver.Dispose();
+            }
+            catch
+            {
+                //driver.Close();
+                driver.Dispose();
+                goto again;
+            }
+        }
+
+
+        private static void SendMail(string subject, string message)
+        {
+            MailMessage objeto_mail = new MailMessage();
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("ribeiro.rs@gmail.com", "c299792458MS");
+            objeto_mail.From = new MailAddress("ribeiro.rs@gmail.com");
+            objeto_mail.To.Add(new MailAddress("ribeiro.rs@gmail.com"));
+            objeto_mail.Subject = subject;
+            objeto_mail.Body = message;
+            client.Send(objeto_mail);
+
+        }
+    }
+}
